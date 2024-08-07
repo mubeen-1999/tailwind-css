f24d69b67224b0fb3bbe37bd0fa6901a869a7582
mubeen-1999 <mubeen.mmc360@gmail.com>
2024-08-07 16:28:17 +0500

    customizing tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// npx tailwindcss init filename --full
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        13:"13px"
+      }
+    },
+  },
+  plugins: [],
+}
+
